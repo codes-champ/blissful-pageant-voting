480cd431c822d6a3b33bf8c0c7f40caad73e750e
Nobles-Dev <145347933+codes-champ@users.noreply.github.com>
2026-05-15 22:48:14 +0100

    Initial project upload

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = "https://mtdgautazahvzojiavfl.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_knl9gsQDJN_uWgboIP6_dQ_JXas7N_1";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
